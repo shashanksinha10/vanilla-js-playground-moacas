3344d5d00d08a12ab49230cf2a7ac29c5ed4ebba
shashanksinha10 <47552910+shashanksinha10@users.noreply.github.com>
2023-08-01 12:28:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -10,27 +10,43 @@ console.log(multiply([1, 2, 3, 4, 5]));
 
 function factorialNumber(n) {
   if (n === 0) {
-      return 1;
+    return 1;
   } else {
-     return n * factorialNumber(n -1);
+    return n * factorialNumber(n - 1);
   }
-  
 }
 
-console.log(factorialNumber(5))
+console.log(factorialNumber(5));
 
 // create an array with the range of number
 
 function rangeOfNumber(start, end) {
-  if (end<start) {
-      return []
+  if (end < start) {
+    return [];
   } else {
-      let numb = rangeOfNumber(start, end-1);
-      numb.push(end)
-      return numb
-
+    let numb = rangeOfNumber(start, end - 1);
+    numb.push(end);
+    return numb;
   }
-
 }
 
-console.log(rangeOfNumber(1,5))
+console.log(rangeOfNumber(1, 5));
+
+// palinmdrome number
+
+function palindromeNumber(n) {
+  if (n.length > 0) {
+    return -1;
+  } else return +n.split('').reverse().join();
+}
+
+console.log(palindromeNumber(1, 2, 1));
+
+// fabonacci number 0,0,1,1,2,3,
+
+function fibRec(n) {
+  if (n <= 1) return n;
+  return fibRec(n - 1) + fibRec(n - 2);
+}
+
+console.log(fibRec(15));
